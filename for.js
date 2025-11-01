7b14676877a9ad08af4335ecfc49d0ddb44280aa
Jefferson Correia Albino <jeff_alb@outlook.com>
2025-11-01 15:04:02 -0400

    Estudo ClubDev

---
+++ for.js
@@ -0,0 +1,8 @@
+let aspasDuplas = "Aspas Duplas";
+let aspasSimples = 'Aspas Simples';
+let templateString = `Template String, OU  Crase`;
+
+
+
+let text = 'Fulando de Tal falou o seguinte: "eu aprendri JS" ' // Para poder usar aspas duplas dentro de aspas simples
+console.log(text);
